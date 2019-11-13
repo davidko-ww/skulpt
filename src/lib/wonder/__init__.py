e344d1e033165f9a67da095e125cb5dfcb27f9e3
David Ko <david.ko@makewonder.com>
2019-11-13 14:52:09 -0800

    Use unique state variables for callbacks

--- src/lib/wonder/__init__.py
+++ src/lib/wonder/__init__.py
@@ -73,6 +73,14 @@ class Robot(impl.RobotImpl):
     def __init__(self, *args, **kwargs):
         super(Robot, self).__init__(*args, **kwargs)
         self._event_handlers = []
+        self._state_vars = {}
+        self._state_var_keys = 0
+
+    def _new_state_var(self):
+        self._state_vars[self._state_var_keys] = 0
+        rc = self._state_var_keys
+        self._state_var_keys += 1
+        return rc
 
     def eye_ring(self, bit_string, brightness):
         # bit_string is a string of 12 ones and zeroes. e.g. "101100111011"
@@ -188,14 +196,15 @@ class Robot(impl.RobotImpl):
         # Add a callback for when the main button is pressed. The callback
         # signature should be:
         #     fn(button_down)
-        self._last_main_button_state = 0
+        state_key = self._new_state_var()
+        self._state_vars[state_key] = 0
         def _cb(sensor_obj):
-            last_button_state = self._last_main_button_state
+            last_button_state = self._state_vars[state_key]
             button_down = sensor_obj['BUTTON_MAIN']['s']
             if last_button_state != button_down:
                 fn(button_down)
-                self._last_main_button_state = button_down
+                self._state_vars[state_key] = button_down
             return True
         self._add_sensor_event_listener(_cb)
-            
+
 
